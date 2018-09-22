c4cf44af0bffeb659524f92976496afb00b52120
Chomwa <cshikati@gmail.com>
2018-09-22 18:18:19 +0200

    e

---
+++ app/src/main/java/com/eafricar/hyke/historyRecyclerView/PayPalConfig.java
@@ -0,0 +1,6 @@
+package com.eafricar.hyke.historyRecyclerView;
+
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID = "ASD9RtnanL9-zK6t_8KKzxr66S-Hp963mu-WhqGHqUnuRyL5tEiiUdcbGV2OHqkpi9iiUcDjsx4M-g9t";
+
+}
